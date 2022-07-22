d0757a92e68f700eb8a9a3be8b96c1ec33fc755b
Sarah Parker <smp3800@gmail.com>
2022-07-22 12:38:03 -0400

    add plot labels

--- exampleFile.R
+++ exampleFile.R
@@ -4,6 +4,7 @@ library(ggplot2)
 
 ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width)) +
   geom_point(color = "Blue") +
+  labs(title = "Iris sepal length vs sepal width") +
   theme()
 
 # change!
