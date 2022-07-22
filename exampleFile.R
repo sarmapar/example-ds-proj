afd9e00735d9b779056c23e85e60c5b40c29a4a5
Sarah Parker <57263711+sarmapar@users.noreply.github.com>
2022-07-22 11:50:41 -0400

    changing color to blue

--- exampleFile.R
+++ exampleFile.R
@@ -3,7 +3,7 @@ print("Hello world!")
 library(ggplot2)
 
 ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width)) +
-  geom_point(color = "Red") +
+  geom_point(color = "Blue") +
   theme()
 
 # change!
